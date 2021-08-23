a9aac7c2fb79d37261e980d30a16d79e63a02dd0
thiago <thiagosteps@gmail.com>
2021-08-23 17:54:33 +0200

    integrations tests finished

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,3 +1,16 @@
-import { createConnection } from 'typeorm';
 
-(async () => await createConnection())();
+import { Connection, createConnection, getConnectionOptions } from "typeorm";
+
+export default async (host = "database"): Promise<Connection> => {
+  const defaultOptions = await getConnectionOptions();
+
+  return createConnection(
+    Object.assign(defaultOptions, {
+      host: process.env.NODE_ENV === "test" ? "localhost" : host,
+      database:
+        process.env.NODE_ENV === "test"
+          ? "fin_api_test"
+          : defaultOptions.database,
+    })
+  );
+};
